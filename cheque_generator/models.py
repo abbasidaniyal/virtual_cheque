82dc032cc646b631335ad06feaa16d241685c603
Daniyal <abbasi.daniyal98@gmail.com>
2019-05-24 03:32:49 +0530

    Updates

--- cheque_generator/models.py
+++ cheque_generator/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Cheque(models.Model):
     issuer = models.ForeignKey("users.User", 
-        verbose_name=_("User that has issued the cheque"),
+        verbose_name=("User that has issued the cheque"),
         on_delete=models.CASCADE)
-    cheque_id = models.UUIDField(_("Check ID"))
+    cheque_id = models.UUIDField(("Check ID"))
     issue_date = models.DateField(auto_now=True,editable=False )
     amount = models.IntegerField()
     cheque = models.ImageField(_(""), 
--- cheque_generator/views.py
+++ cheque_generator/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from .forms import UserForm
+from .forms import ChequeForm
 from .models import Users
 # Create your views here.
-def create_user(request):
-    form = UserForm(request.POST or none)
-    if from.is_valid():
-        form.save()
 
-    context = {
-        'form': form
-    }
+
+def create_user(request):
+    form = ChequeForm(request.POST)
+    if request.method =='POST':
+        form=ChequeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context = {
+            'form': form
+            }   
+    else:
+        form=ChequeForm()
+        
     return render(request, "user/user_create.html",context)
     
